9ca9e0ca6943a9d04c468be299d471966d05d987
HappyWind6789 <phongwind.hy@gmail.com>
2024-01-19 18:30:34 +0700

    Phuong trinh bac 2

---
+++ Baithuchanh/src/phuongtrinhbac2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class phuongtrinhbac2 {
+    public static void main(String[] args) {
+        double a, b, c, x1, x2, delta;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap a = ");
+        a = sc.nextDouble();
+        System.out.print("Nhap b = ");
+        b = sc.nextDouble();
+        System.out.print("Nhap c = ");
+        c = sc.nextDouble();
+        if (a == 0) {System.out.println("Khong phai phuong trinh bac2");}
+        else {
+            delta = Math.pow(b, 2) - 4*a*c ;
+            if (delta < 0 ) System.out.println("Phuong trinh vo nghiem");
+            else if (delta == 0) {
+                x1=x2= -b / 2*a ;
+                System.out.println("Phuong trinh co nghiem kep x1=x2= "+x1);
+            } else {
+                x1 = -b + Math.sqrt(delta) / 2*a ;
+                x2 = -b - Math.sqrt(delta) / 2*a ;
+                System.out.println("Phuong trinh co 2 nghiem x1= "+x1 + ", x2 = "+ x2);
+            }
+        }
+    }
+}
