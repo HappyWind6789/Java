15fb5e9116930cf6cda71d2ea7a4b93e7e59795d
HappyWind6789 <phongwind.hy@gmail.com>
2024-01-26 21:21:09 +0700

    lab2slide55

---
+++ Baithuchanh/src/lab2slide55.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class lab2slide55 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i <= 20 ; i++)
+        if (i % 5 == 0) {
+            System.out.println("Cac so chia het cho 5 la : " + i);
+        }
+    }
+}
