452721eb143c3eab5baaeca57fb16f343750d003
HappyWind6789 <phongwind.hy@gmail.com>
2024-01-26 09:36:13 +0700

    lab2slide30

---
+++ Baithuchanh/src/lab2slide30.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class lab2slide30 {
+    public static void main(String[] args) {
+        long n, sum=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap n = ");
+        n = sc.nextLong();
+            if (n < 10) {
+                sum = sum + n ;
+            } else {
+                for (int i=1; i<=n; i++){
+                sum = sum + n%10 ;
+                n = n/10 ;
+            }
+         }
+         System.out.print("tong la :"+ sum);
+    }
+}
