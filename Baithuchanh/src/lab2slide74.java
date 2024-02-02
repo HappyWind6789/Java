224c41b346b97e4e3228f3bae7a86c4bcc6fe95f
HappyWind6789 <phongwind.hy@gmail.com>
2024-02-02 22:13:56 +0700

    lab2slide74

--- Baithuchanh/src/lab2slide74.java
+++ Baithuchanh/src/lab2slide74.java
@@ -1,18 +1,17 @@
-import java.lang.reflect.Array;
 import java.util.Scanner;
-
 public class lab2slide74 {
     public static void main(String[] args) {
-        int n,i, sum = 0 ;
+        double sum = 0 ;
+        int i, n;
         Scanner sc = new Scanner(System.in);
         do {
             System.out.print("Nhap so phan tu mang = ");
             n = sc.nextInt();
         } while (n < 0);
-        int so[]= new int[n];
+        Double so[]= new Double[n];
         for (i = 0; i < n; i++){
             System.out.print("Nhap vao phan tu thu " + i +" = " );
-            so[i] = sc.nextInt();
+            so[i] = sc.nextDouble();
         }
         for ( i = 0 ; i < n; i++){
             if ( so[i] % 2 == 0 )
